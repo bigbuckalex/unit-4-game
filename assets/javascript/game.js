386bf4043f62fe33814212d7d5429fe90353d14a
bigbuckalex <bigbuckalex@gmail.com>
2019-03-02 18:48:50 -0500

    html layout done

---
+++ assets/javascript/game.js
@@ -0,0 +1,29 @@
+let trump = {
+    health: 100,
+    attack = 20,
+    counter = 30
+}
+
+let obama = {
+    health: 100,
+    attack = 20,
+    counter = 30
+}
+
+let bush = {
+    health: 100,
+    attack = 20,
+    counter = 30
+}
+
+let clinton = {
+    health: 100,
+    attack = 20,
+    counter = 30
+}
+
+$("#trump-health").html = trump.health;
+
+let charList = [trump, obama, bush, clinton];
+
+let enemyList = [];
