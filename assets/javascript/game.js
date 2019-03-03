d34b2d1d539d7b38ca8eedd346b6f7c1ecc7f31a
bigbuckalex <bigbuckalex@gmail.com>
2019-03-02 21:56:52 -0500

    Finished html and css, started on javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,29 +1,56 @@
+$(document).ready(function(){
+
 let trump = {
+    name: "trump",
     health: 100,
-    attack = 20,
-    counter = 30
+    attack: 20,
+    counter: 30
 }
 
 let obama = {
     health: 100,
-    attack = 20,
-    counter = 30
+    attack: 20,
+    counter: 30
 }
 
 let bush = {
     health: 100,
-    attack = 20,
-    counter = 30
+    attack: 20,
+    counter: 30
 }
 
 let clinton = {
     health: 100,
-    attack = 20,
-    counter = 30
+    attack: 20,
+    counter: 30
 }
-
-$("#trump-health").html = trump.health;
 
 let charList = [trump, obama, bush, clinton];
 
 let enemyList = [];
+
+let commander;
+
+$("#trump-health").text(trump.health);
+$("#obama-health").text(obama.health);
+$("#bush-health").text(bush.health);
+$("#clinton-health").text(clinton.health);
+
+$("#trump-card").data(trump);
+$("#obama-card").data(obama);
+$("#bush-card").data(bush);
+$("#clinton-card").data(clinton);
+
+
+if(enemyList.length===0){
+    $(".card-image").on("click", function(){
+        // commander = this.data();
+        console.log(event.target.parentNode.parentNode);
+        // enemyList = ;
+        $(".card").hide();
+    })
+}
+
+console.log(trump.health);
+
+})
